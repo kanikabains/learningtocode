c59ab5f2528a3b7ebb2396de355f2bb54afdf014
kanikabains <119393112+kanikabains@users.noreply.github.com>
2022-12-08 15:20:33 +0530

    Add files via upload

---
+++ Javascript/6-Function/function.js
@@ -0,0 +1,14 @@
+function greet(name, greetText){
+    console.log(greetText + " " + name);
+    console.log(name );
+}
+let name = "abc";
+let name1 = "bcb";
+let name2 = "bcb";
+let name3 = "nsd";
+let greetText = "Good Morning";
+greet(name, greetText);
+greet(name1, greetText);
+greet(name2, greetText);
+    
+
