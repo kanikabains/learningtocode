8ad9d69c6517de1feec39b885d0b005be79e14b7
kanikabains <119393112+kanikabains@users.noreply.github.com>
2022-12-12 11:09:20 +0530

    Add files via upload

---
+++ Javascript/6-Function/fuction argument.js
@@ -0,0 +1,7 @@
+function sum(a , b){ // passing parameters
+    var total = a+b;
+    console.log(a+b);
+}
+sum();
+sum(20,20);  // arguments...
+sum(50,50);
--- Javascript/6-Function/function.js
+++ Javascript/6-Function/function.js
@@ -1,14 +1,6 @@
-function greet(name, greetText){
-    console.log(greetText + " " + name);
-    console.log(name );
+function sum(){  // function definition.....
+    var a=20, b= 30;
+    var total = a+b;
+    console.log(total);
 }
-let name = "abc";
-let name1 = "bcb";
-let name2 = "bcb";
-let name3 = "nsd";
-let greetText = "Good Morning";
-greet(name, greetText);
-greet(name1, greetText);
-greet(name2, greetText);
-    
-
+sum();   // function calling...
