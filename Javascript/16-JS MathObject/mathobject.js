a5c3e82afaf649027979f6f158e3a18f5d266ec7
kanikabains <119393112+kanikabains@users.noreply.github.com>
2022-12-12 16:43:22 +0530

    Add files via upload

---
+++ Javascript/16-JS MathObject/mathobject.js
@@ -0,0 +1,33 @@
+// (Math.PI)
+console.log(Math.PI);
+
+// Math.round() rounded to its nearest interger
+let num = 10.8876;
+console.log(Math.round(num));
+
+//Math.pow()
+console.log(Math.pow(2,3));
+
+// Math.sqrt()
+console.log(Math.sqrt(20));
+
+//Math.abs()
+console.log(Math.abs(-55.87));
+
+// Math.ceil()
+console.log(Math.ceil(4.2));
+
+// Math.floor()
+console.log(Math.floor(4.7));
+
+//   Math.max()
+console.log(Math.max(2,3,5,-3,8));
+
+// Math.min()
+console.log(Math.min(2,3,5,-3,8));
+
+// Math.random
+console.log(Math.floor(Math.random()*10));
+
+// Math.trunc()
+console.log(Math.trunc(2.6));
