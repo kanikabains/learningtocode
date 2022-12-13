73ceb6519b4937d7137a4e43c05367914d7a5924
kanikabains <119393112+kanikabains@users.noreply.github.com>
2022-12-13 17:53:25 +0530

    Delete escapecharacter.js

--- Javascript/2-String/escapecharacter.js
+++
@@ -1,7 +0,0 @@
-let anySentence = 'we are the so-called "vikings" from north';
-console.log(anySentence);
-
-// Also
-
-let anySentence1 = 'we are the so-called "vikings" from north';
-console.log(anySentence1);
